c0058dac11279ba1e05c381653c17b5f5d0a18cd
alexander <emyleigh@inbox.ru>
2022-11-19 12:22:57 +0300

    add functionality to format date

---
+++ src/convertDate.js
@@ -0,0 +1,15 @@
+const defaultOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    weekday: 'long',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+};
+
+export default (date, optoins = defaultOptions) => {
+    const newDate = new Date(date)
+    return newDate.toLocaleDateString('en-US', optoins)
+}
