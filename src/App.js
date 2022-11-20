3dba0d4b135c7c77a5f2373bbb0079b12babe766
alexander <emyleigh@inbox.ru>
2022-11-20 23:10:01 +0300

    add routing and refactor process of initialization of the application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import {
-  createBrowserRouter,
-  RouterProvider,
   Routes,
   Route,
-  Router
 } from "react-router-dom";
+import Layout from "./components/Layout";
 import NewsPage from './components/NewsPage';
 import StoryPage from './components/StoryPage';
+import CategoryPage from "./components/CategoryPage";
+import CategoriesListPage from "./components/CategoriesListPage";
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path='/' element={<NewsPage />} />
-      <Route path='/:id' element={<StoryPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<NewsPage />} />
+        <Route path='/:id' element={<StoryPage />} />
+        <Route path='/categories' element={<CategoriesListPage />} />
+        <Route path="/categories/:categoryName" element={<CategoryPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
     </Routes>
     </>
   );
